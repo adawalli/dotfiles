d1e0e3d1a02a6873c64a3f501c1f7a93edcd644a
Adam Wallis <adam@byte.fit>
2025-09-30 18:57:17 -0400

    feat: Fix KCN

--- dot_config/fish/completions/kcn.fish
+++
@@ -1,4 +0,0 @@
-# Completions for kcn (kubectl config set-context namespace)
-
-# Complete with available namespaces from current context
-complete -c kcn -f -a '(kubectl get namespaces -o jsonpath="{.items[*].metadata.name}" 2>/dev/null | string split " ")'
