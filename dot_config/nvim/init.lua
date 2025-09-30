34d0f3e6187a1977f79229c957a62c9cec73b0d8
Adam Wallis <adam@byte.fit>
2025-09-30 18:08:06 -0400

    feat: Revert easier min.deps

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -1,19 +1,3 @@
--- Bootstrap mini.deps
--- Compatibility: Older Neovim versions (<0.10) don't support stdpath('log')
--- Check if we need to add a fallback
-local has_log_stdpath = pcall(vim.fn.stdpath, 'log')
-if not has_log_stdpath then
-  -- Monkey-patch stdpath to support 'log' for older Neovim versions
-  local original_stdpath = vim.fn.stdpath
-  vim.fn.stdpath = function(what)
-    if what == 'log' then
-      return original_stdpath('data')
-    end
-    return original_stdpath(what)
-  end
-end
-
--- Clone 'mini.nvim' manually in a way that it gets managed by 'mini.deps'
 local path_package = vim.fn.stdpath('data') .. '/site/'
 local mini_path = path_package .. 'pack/deps/start/mini.nvim'
 if not vim.loop.fs_stat(mini_path) then
@@ -28,15 +12,7 @@ if not vim.loop.fs_stat(mini_path) then
 end
 
 -- Set up 'mini.deps' (customize to your liking)
--- Use stdpath('data') for log to support older Neovim versions (pre-0.10)
-require('mini.deps').setup({
-  path = {
-    package = path_package,
-    snapshot = vim.fn.stdpath('config') .. '/mini-deps-snap',
-    log = vim.fn.stdpath('data') .. '/mini-deps.log',
-  }
-})
-
+require('mini.deps').setup({ path = { package = path_package } })
 -- Helper for adding plugins
 local add = require('mini.deps').add
 
