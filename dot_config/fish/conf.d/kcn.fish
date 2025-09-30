d406c3aa38293bb9265777a11c62fa14e0ae34de
Adam Wallis <adam@byte.fit>
2025-09-30 19:37:25 -0400

    bug: Fix kcn completions Please enter the commit message for your changes. Lines starting

--- dot_config/fish/conf.d/kcn.fish
+++ dot_config/fish/conf.d/kcn.fish
@@ -1,18 +1,5 @@
-# Custom function to set namespace for current kubectl context
-# This evaluates the current context dynamically, not at shell startup
+# Custom abbreviation to set namespace for current kubectl context
+# Using --current instead of $(kubectl config current-context) to avoid
+# static evaluation at shell startup
 
-function kcn -d "Set namespace for current kubectl context"
-    if test (count $argv) -eq 0
-        echo "Usage: kcn <namespace>"
-        return 1
-    end
-
-    set -l current_context (kubectl config current-context 2>/dev/null)
-
-    if test -z "$current_context"
-        echo "Error: No current kubectl context found"
-        return 1
-    end
-
-    kubectl config set-context $current_context --namespace $argv[1]
-end
+abbr -a -g kcn kubectl config set-context --current --namespace
