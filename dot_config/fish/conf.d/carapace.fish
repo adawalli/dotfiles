eece02fff547dcc37903161cb69c5830c0fa0871
Adam Wallis <awallis@nvidia.com>
2026-01-22 12:42:59 -0500

    chore: exclude cat and ls from carapace completions
    Add cat and ls to CARAPACE_EXCLUDES to prevent carapace from providing completions for these basic commands where the native completions are preferred.

--- dot_config/fish/conf.d/carapace.fish
+++ dot_config/fish/conf.d/carapace.fish
@@ -1,5 +1,5 @@
 if status is-interactive; and command -q carapace
     set -q CARAPACE_BRIDGES; or set -Ux CARAPACE_BRIDGES 'zsh,fish,bash,inshellisense'
-    set -q CARAPACE_EXCLUDES; or set -Ux CARAPACE_EXCLUDES kubectl
+    set -q CARAPACE_EXCLUDES; or set -Ux CARAPACE_EXCLUDES kubectl,cat,ls
     carapace _carapace | source
 end
